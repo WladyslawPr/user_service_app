016cd4b986cb23841e17895e35d9c40ba16f5005
vlpr <user.waw3@gmail.com>
2023-03-09 17:54:48 +0100

    adding db source and change version app

---
+++ src/main/java/com/example/user_service_app/api/UserResource.java
@@ -0,0 +1,25 @@
+package com.example.user_service_app.api;
+
+import com.example.user_service_app.domain.User;
+import com.example.user_service_app.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class UserResource {
+    private final UserService userService;
+
+    public UserResource(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
+}
