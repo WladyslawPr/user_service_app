8806af01dc510927e54d3d30e747785cbfb37b91
vlpr <user.waw3@gmail.com>
2023-03-10 15:21:03 +0100

    adding test users

--- src/main/java/com/example/user_service_app/UserServiceAppApplication.java
+++ src/main/java/com/example/user_service_app/UserServiceAppApplication.java
@@ -1,13 +1,43 @@
 package com.example.user_service_app;
 
+import com.example.user_service_app.domain.Role;
+import com.example.user_service_app.domain.User;
+import com.example.user_service_app.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserServiceAppApplication {
 
     public static void main (String[] args) {
         SpringApplication.run(UserServiceAppApplication.class, args);
+    }
+    @Bean
+    CommandLineRunner commandLineRunner(UserService userService) {
+        return args -> {
+            userService.saveRole(new Role(null, "ROLE_USER"));
+            userService.saveRole(new Role(null, "ROLE_MANAGER"));
+            userService.saveRole(new Role(null, "ROLE_ADMIN"));
+            userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+            userService.saveUser(new User(null, "Vl Pr", "Vl", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Peter K", "Peter", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Ray L", "Ray", "1234", new ArrayList<>()));
+            userService.saveUser(new User(null, "Ivan S", "Ivan", "1234", new ArrayList<>()));
+
+            userService.addRoleToUser("Vl", "ROLE_USER");
+            userService.addRoleToUser("Vl", "ROLE_MANAGER");
+            userService.addRoleToUser("Vl", "ROLE_ADMIN");
+            userService.addRoleToUser("Peter", "ROLE_MANAGER");
+            userService.addRoleToUser("Ray", "ROLE_ADMIN");
+            userService.addRoleToUser("Ivan", "ROLE_SUPER_ADMIN");
+            userService.addRoleToUser("Ivan", "ROLE_USER");
+            userService.addRoleToUser("Ivan", "ROLE_ADMIN");
+        };
     }
 
 }
--- src/main/java/com/example/user_service_app/api/UserResource.java
+++ src/main/java/com/example/user_service_app/api/UserResource.java
@@ -1,12 +1,14 @@
 package com.example.user_service_app.api;
 
+import com.example.user_service_app.domain.Role;
 import com.example.user_service_app.domain.User;
 import com.example.user_service_app.service.UserService;
+import lombok.Data;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -22,4 +24,27 @@ public class UserResource {
     public ResponseEntity<List<User>> getUsers() {
         return ResponseEntity.ok().body(userService.getUsers());
     }
+    @PostMapping("/user/save")
+    public ResponseEntity<User> saveUser(@RequestBody User user ) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toString());
+        return ResponseEntity.created(uri).body(userService.saveUser(user));
+    }
+    @PostMapping("/role/save")
+    public ResponseEntity<Role> saveRole(@RequestBody Role role) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("api/role/save").toString());
+        return ResponseEntity.created(uri).body(userService.saveRole(role));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
+        userService.addRoleToUser(form.getUsername(), form.getRoleName());
+        return ResponseEntity.ok().build();
+    }
+
+}
+
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
 }
